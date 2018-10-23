3b4e60987f500f406df313cec1e339cbf6a35bd1
HuyTrinh <huy@makehelsinki.com>
2018-10-23 10:28:23 +0300

    refactor code

--- src/store/saga/index.js
+++ src/store/saga/index.js
@@ -1,15 +1,17 @@
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery, takeLatest, all} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import * as positiveBoxSaga from './positivebox';
 import * as infoSaga from './info';
 import * as projectSaga from './project';
 
 export function* rootSaga() {
-  yield takeEvery(actionTypes.FETCH_INFO_START, infoSaga.fetchInfoSaga)
-  yield takeEvery(actionTypes.FETCH_INFO_START, infoSaga.fetchExpSaga)
+  yield all([
+    takeEvery(actionTypes.FETCH_INFO_START, infoSaga.fetchInfoSaga),
+    takeEvery(actionTypes.FETCH_INFO_START, infoSaga.fetchExpSaga),
 
-  yield takeEvery(actionTypes.FETCH_POSITIVE_START, positiveBoxSaga.fetchPositiveSaga)
-  yield takeEvery(actionTypes.POST_POSITIVE_START, positiveBoxSaga.postPositivesSaga)
+    takeEvery(actionTypes.FETCH_POSITIVE_START, positiveBoxSaga.fetchPositiveSaga),
+    takeLatest(actionTypes.POST_POSITIVE_START, positiveBoxSaga.postPositivesSaga),
 
-  yield takeEvery(actionTypes.FETCH_PROJECTS_START, projectSaga.fetchProjectSaga)
+    takeEvery(actionTypes.FETCH_PROJECTS_START, projectSaga.fetchProjectSaga)
+  ]);
 }
