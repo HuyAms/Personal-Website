55b66662e8e2223a3e5d48017119b7c6f48cd3e7
HuyTrinh <dinhhuyams@gmail.com>
2018-10-24 05:46:27 +0300

    refactor saga, using call

--- src/store/saga/project.js
+++ src/store/saga/project.js
@@ -1,11 +1,11 @@
-import {put} from 'redux-saga/effects';
+import {put, call} from 'redux-saga/effects';
 import * as actions from '../actions/project';
 import axios from "../../axious";
 
 export function* fetchProjectSaga(action) {
   try {
 
-    const response = yield axios.get('/projects.json', action.positiveData);
+    const response = yield call(axios.get, '/projects.json', action.positiveData);
 
     const fetchedProject = [];
     for (let key in response.data) {
